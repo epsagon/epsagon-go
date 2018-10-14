0d99815996e25c4c60a8c531ed243c28f88ec7bc
Erez Freiberger <erez@epsagon.com>
2018-10-14 23:41:33 +0300

    initial support in wrapping lambda functions

---
+++ example/simple_lambda/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/epsagon/epsagon-go/epsagon"
+	"log"
+)
+
+func myHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println("In myHandler, received body: ", request.Body)
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+}
+
+func main() {
+	log.Println("enter main")
+	lambda.Start(epsagon.WrapLambdaHandler(
+		"erez-test-go",
+		"4e988052-fb55-4c6d-b46f-f17f3dd81b6b",
+		"http://dev.tc.epsagon.com",
+		myHandler,
+	))
+}
