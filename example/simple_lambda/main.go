647a2117b634f5bb5972ef6be5c797cb385b01d8
henprtz <henprtz@gmail.com>
2018-11-01 11:07:17 +0200

    fix

--- example/simple_lambda/main.go
+++ example/simple_lambda/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/epsagon/epsagon-go/epsagon"
@@ -10,9 +9,8 @@ import (
 
 type Response events.APIGatewayProxyResponse
 
-func Handler(ctx context.Context, event interface{}) (Response, error) {
-	log.Println("In myHandler, received body: ", ctx)
-	log.Println("In myHandler, received body: ", event)
+func myHandler(request events.APIGatewayProxyRequest) (Response, error) {
+	log.Println("In myHandler, received body: ", request.Body)
 	return Response {Body: "yes", StatusCode: 200}, nil
 }
 
@@ -21,5 +19,5 @@ func main() {
 	config := epsagon.Config{
 		ApplicationName: "erez-test-go",
 		CollectorURL:    "http://dev.tc.epsagon.com"}
-	lambda.Start(epsagon.WrapLambdaHandler(&config, Handler))
+	lambda.Start(epsagon.WrapLambdaHandler(&config, myHandler))
 }
