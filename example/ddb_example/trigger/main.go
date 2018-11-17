1f9681dbe294bdde0b52dbec3becf3d9b29fd4d2
henprtz <henprtz@gmail.com>
2018-11-17 19:04:53 +0200

    add ddb example

---
+++ example/ddb_example/trigger/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/epsagon/epsagon-go/epsagon"
+	"log"
+)
+
+func ddbHandler(ddbEvent events.DynamoDBEvent) error {
+	log.Println("In mySQSHandler, received body: ", ddbEvent)
+	return nil
+}
+
+func main() {
+	log.Println("enter main")
+	config := epsagon.Config{
+		ApplicationName: "ddb-test-go",
+	}
+	lambda.Start(epsagon.WrapLambdaHandler(&config, ddbHandler))
+	log.Println("exit main")
+}
---
+++ example/ddb_example/write/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/epsagon/epsagon-go/epsagon"
+	"github.com/epsagon/epsagon-go/wrappers/aws/aws-sdk-go/aws"
+	"github.com/satori/go.uuid"
+	"log"
+	"os"
+)
+
+type Item struct {
+	item string`json:"item"`
+}
+
+func ddbHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println("In ddbHandler, received body: ", request.Body)
+
+	session := epsagonawswrapper.WrapSession(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)))
+
+	svc := dynamodb.New(session)
+
+	item := Item{
+		item: uuid.NewV4().String(),
+	}
+
+	av, marshalErr := dynamodbattribute.MarshalMap(item)
+
+	if marshalErr != nil {
+		marshErrMsg := fmt.Sprintf("Failed to marshal table: %v", marshalErr)
+		log.Println(marshErrMsg)
+		return events.APIGatewayProxyResponse{Body: marshErrMsg, StatusCode: 500}, marshalErr
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item: av,
+		TableName: aws.String(os.Getenv("TABLE_NAME")),
+	}
+
+	_, err := svc.PutItem(input)
+
+	if err != nil {
+		fmt.Println("Failed calling PutItem:")
+		errMsg := fmt.Sprintf("Failed calling PutItem: %v", err.Error())
+		return events.APIGatewayProxyResponse{Body: errMsg, StatusCode: 500}, err
+	}
+
+	fmt.Println("Successfully written item to table")
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+}
+
+func main() {
+	log.Println("enter main")
+	config := epsagon.Config{
+		ApplicationName: "ddb-test-go",
+		Debug:           true,
+	}
+	lambda.Start(epsagon.WrapLambdaHandler(&config, ddbHandler))
+	log.Println("exit main")
+}
