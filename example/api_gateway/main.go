8837480b1c8477b50bce7208ed70963c04ddaabc
Erez Freiberger <erez@epsagon.com>
2018-11-18 16:05:14 +0200

    add api gateway and http example

--- example/api_gateway/main.go
+++ example/api_gateway/main.go
@@ -4,7 +4,10 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/epsagon/epsagon-go/epsagon"
+	"github.com/epsagon/epsagon-go/wrappers/net/http"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 // Response is an API gateway response type
@@ -12,7 +15,19 @@ type Response events.APIGatewayProxyResponse
 
 func myHandler(request events.APIGatewayProxyRequest) (Response, error) {
 	log.Println("In myHandler, received body: ", request.Body)
-	return Response{Body: "yes", StatusCode: 200}, nil
+	client := epsagonhttp.Wrap(http.Client{})
+	resp, err := client.Get("https://api.randomuser.me")
+	var body string
+	if err == nil {
+		defer resp.Body.Close()
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err == nil {
+			body = string(bodyBytes)
+		}
+	} else {
+		log.Printf("Error in getting response: %+v\n", err)
+	}
+	return Response{Body: "yes: random user: " + body, StatusCode: 200}, nil
 }
 
 func main() {
