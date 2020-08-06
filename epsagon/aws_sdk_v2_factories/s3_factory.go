cd4ae4afb9c846821f8f8bf42a7448a943b8d74a
Erez Freiberger <enoodle@gmail.com>
2020-08-06 13:54:45 +0300

    Adding aws sdk v2 s3 (#41)
    * adding s3 support for aws sdk v2
    
    * adding exmaple for aws sdk v2 with s3

---
+++ epsagon/aws_sdk_v2_factories/s3_factory.go
@@ -0,0 +1,82 @@
+package epsagonawsv2factories
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/epsagon/epsagon-go/protocol"
+	"reflect"
+	"strings"
+	"time"
+)
+
+// S3EventDataFactory creats an Epsagon Resource from aws.Request to S3
+func S3EventDataFactory(r *aws.Request, res *protocol.Resource, metadataOnly bool) {
+	inputValue := reflect.ValueOf(r.Params).Elem()
+	getResourceNameFromField(res, inputValue, "Bucket")
+	handleSpecificOperations := map[string]specificOperationHandler{
+		"HeadObject":  handleS3GetOrHeadObject,
+		"GetObject":   handleS3GetOrHeadObject,
+		"PutObject":   handleS3PutObject,
+		"ListObjects": handleS3ListObject,
+	}
+	handleSpecificOperation(r, res, metadataOnly, handleSpecificOperations, nil)
+}
+
+func commonS3OpertionHandler(r *aws.Request, res *protocol.Resource, metadataOnly bool) {
+	inputValue := reflect.ValueOf(r.Params).Elem()
+	updateMetadataFromValue(inputValue, "Key", "key", res.Metadata)
+	outputValue := reflect.ValueOf(r.Data).Elem()
+	etag, ok := getFieldStringPtr(outputValue, "ETag")
+	if ok {
+		etag = strings.Trim(etag, "\"")
+		res.Metadata["etag"] = etag
+	}
+}
+
+func handleS3GetOrHeadObject(r *aws.Request, res *protocol.Resource, metadataOnly bool) {
+	commonS3OpertionHandler(r, res, metadataOnly)
+	outputValue := reflect.ValueOf(r.Data).Elem()
+	updateMetadataFromValue(outputValue, "ContentLength", "file_size", res.Metadata)
+
+	lastModifiedField := outputValue.FieldByName("LastModified")
+	if lastModifiedField == (reflect.Value{}) {
+		return
+	}
+	lastModified := lastModifiedField.Elem().Interface().(time.Time)
+	res.Metadata["last_modified"] = lastModified.String()
+}
+
+func handleS3PutObject(r *aws.Request, res *protocol.Resource, metadataOnly bool) {
+	commonS3OpertionHandler(r, res, metadataOnly)
+}
+
+type s3File struct {
+	key  string
+	size int64
+	etag string
+}
+
+func handleS3ListObject(r *aws.Request, res *protocol.Resource, metadataOnly bool) {
+	if metadataOnly {
+		return
+	}
+
+	outputValue := reflect.ValueOf(r.Data).Elem()
+	contentsField := outputValue.FieldByName("Contents")
+	if contentsField == (reflect.Value{}) {
+		return
+	}
+	length := contentsField.Len()
+	files := make([]s3File, length)
+	for i := 0; i < length; i++ {
+		var key, etag string
+		var size int64
+		fileObject := contentsField.Index(i).Elem()
+		etag = fileObject.FieldByName("ETag").Elem().String()
+		key = fileObject.FieldByName("Key").Elem().String()
+		size = fileObject.FieldByName("Size").Elem().Int()
+
+		files = append(files, s3File{key, size, etag})
+	}
+	res.Metadata["files"] = fmt.Sprintf("%+v", files)
+}
