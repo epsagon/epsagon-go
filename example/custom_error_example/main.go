a6042871ac521826d198b372cdcdf8a48ea9b369
Maor Levi <maor@epsagon.com>
2020-09-30 15:18:56 +0300

    Adding custom error example code

---
+++ example/custom_error_example/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/epsagon/epsagon-go/epsagon"
+)
+
+func doTask(a int, b string) (int, error) {
+	log.Printf("inside doTask: b = %s", b)
+	if a < 5 {
+		// This error with its type will be viewable in Epsagon dashboard
+		epsagon.ErrorWithType("value must be bigger than 5", "InputError")
+	}
+
+	return a + 1, fmt.Errorf("boom")
+}
+
+func main() {
+	// Normal call
+	res, err := doTask(3, "world")
+	if err != nil {
+		log.Printf("First result is %d", res)
+	} else {
+		log.Printf("error was: %v", err)
+	}
+
+	// With Epsagon instrumentation
+	config := epsagon.NewTracerConfig("generic-go-wrapper", "")
+	config.Debug = true
+	response := epsagon.GoWrapper(config, doTask)(5, "hello")
+	res2 := response[0].Int()
+	errInterface := response[1].Interface()
+	if errInterface == nil {
+		log.Printf("Result was %d", res2)
+	} else {
+		err := errInterface.(error)
+		log.Printf("error was: %v", err)
+	}
+}
