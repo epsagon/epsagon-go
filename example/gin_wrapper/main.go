936201b954c52a5b416be1476e21f3a78882cd9d
Erez Freiberger <erez@epsagon.com>
2020-10-29 19:40:26 +0200

    improving gin example with nested instrumentation

--- example/gin_wrapper/main.go
+++ example/gin_wrapper/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"time"
 
 	"github.com/epsagon/epsagon-go/epsagon"
@@ -13,22 +14,43 @@ import (
 
 func main() {
 	// r := gin.Default()
+	config := epsagon.NewTracerConfig(
+		"erez-test-gin", "",
+	)
+	config.MetadataOnly = false
 	r := epsagongin.GinRouterWrapper{
 		IRouter:  gin.Default(),
 		Hostname: "my_site",
-		Config: epsagon.NewTracerConfig(
-			"erez-test-gin", "",
-		),
+		Config:   config,
 	}
 
+	r.GET("/ping", func(c *gin.Context) {
+		time.Sleep(time.Second * 1)
+		fmt.Println("hello world")
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	r.POST("/ping", func(c *gin.Context) {
 		time.Sleep(time.Second * 1)
 		body, err := ioutil.ReadAll(c.Request.Body)
 		if err == nil {
-			fmt.Println(body)
+			fmt.Println("Recieved body: ", string(body))
 		} else {
 			fmt.Println("Error reading body: ", err)
 		}
+
+		client := http.Client{
+			Transport: epsagonhttp.NewTracingTransport(epsagongin.EpsagonContext(c))}
+		resp, err := client.Get("http://example.com")
+
+		if err == nil {
+			respBody, err := ioutil.ReadAll(resp.Body)
+			if err == nil {
+				fmt.Println("First 1000 bytes recieved: ", string(respBody[:1000]))
+			}
+		}
+
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
