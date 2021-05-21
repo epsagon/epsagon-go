3f9507821a7dec174e9d04f07969e0708ad94cc5
ronnathaniel <ronnathaniel>
2021-05-21 16:00:59 -0400

    fix(s3): use new AWSCall & AWSClient

--- epsagon/aws_sdk_v2_factories/s3_factory.go
+++ epsagon/aws_sdk_v2_factories/s3_factory.go
@@ -2,63 +2,105 @@ package epsagonawsv2factories
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/epsagon/epsagon-go/protocol"
 	"github.com/epsagon/epsagon-go/tracer"
 	"reflect"
-	"strings"
-	"time"
 )
 
 // S3EventDataFactory creats an Epsagon Resource from aws.Request to S3
 func S3EventDataFactory(
-	r *aws.Request,
+	r *AWSCall,
 	res *protocol.Resource,
 	metadataOnly bool,
 	currentTracer tracer.Tracer,
 ) {
-	inputValue := reflect.ValueOf(r.Params).Elem()
+	inputValue := reflect.ValueOf(r.Req).Elem()
+
+	fmt.Println("\nINPUT value s3 create event::")
+	fmt.Println(inputValue)
+
 	getResourceNameFromField(res, inputValue, "Bucket")
+
 	handleSpecificOperations := map[string]specificOperationHandler{
-		"HeadObject":  handleS3GetOrHeadObject,
-		"GetObject":   handleS3GetOrHeadObject,
+		"HeadObject":  handleS3HeadObject,
+		"GetObject":   handleS3GetObject,
 		"PutObject":   handleS3PutObject,
 		"ListObjects": handleS3ListObject,
 	}
 	handleSpecificOperation(r, res, metadataOnly, handleSpecificOperations, nil, currentTracer)
 }
 
-func commonS3OpertionHandler(r *aws.Request, res *protocol.Resource, metadataOnly bool) {
-	inputValue := reflect.ValueOf(r.Params).Elem()
-	updateMetadataFromValue(inputValue, "Key", "key", res.Metadata)
-	outputValue := reflect.ValueOf(r.Data).Elem()
-	etag, ok := getFieldStringPtr(outputValue, "ETag")
-	if ok {
-		etag = strings.Trim(etag, "\"")
-		res.Metadata["etag"] = etag
-	}
+func commonS3OpertionHandler(r *AWSCall, res *protocol.Resource, metadataOnly bool) {
+	//inputValue := reflect.ValueOf(r.Req).Elem()
+	//updateMetadataFromValue(inputValue, "Key", "key", res.Metadata)
+	//outputValue := reflect.ValueOf(r.Res).Elem()
+	//etag, ok := getFieldStringPtr(outputValue, "ETag")
+	//if ok {
+	//	etag = strings.Trim(etag, "\"")
+	//	res.Metadata["etag"] = etag
+	//}
+}
+
+func handleS3GetObject(
+	r *AWSCall,
+	res *protocol.Resource,
+	metadataOnly bool,
+	t tracer.Tracer,
+) {
+	// try opening req body
+
+	//fmt.Println("OPENING GET OBJ BODY")
+	//b := r.Res.Request.Body
+	//fmt.Println(b)
+	//body, err := ioutil.ReadAll(b)
+	//fmt.Println("OPENING GET OBJ BODY")
+	//
+	//defer r.Req.Body.Close()
+	//if err != nil {
+	//	fmt.Println("could not read body")
+	//	fmt.Println(err)
+	//}
+	//
+	//fmt.Println("UNmarshalling")
+	//var s3HeadObjInput s3.HeadObjectInput
+	//err = json.Unmarshal(body, &s3HeadObjInput)
+	//if err != nil {
+	//	fmt.Println("could not head object")
+	//}
+	fmt.Println("GOT TO BOTTOM OF GET")
+	commonS3OpertionHandler(r, res, metadataOnly)
+	handleS3GetOrHeadObject(r, res, metadataOnly, t)
+}
+
+func handleS3HeadObject(
+	r *AWSCall,
+	res *protocol.Resource,
+	metadataOnly bool,
+	t tracer.Tracer,
+) {
+	commonS3OpertionHandler(r, res, metadataOnly)
+	handleS3GetOrHeadObject(r, res, metadataOnly, t)
 }
 
 func handleS3GetOrHeadObject(
-	r *aws.Request,
+	r *AWSCall,
 	res *protocol.Resource,
 	metadataOnly bool,
 	_ tracer.Tracer,
 ) {
-	commonS3OpertionHandler(r, res, metadataOnly)
-	outputValue := reflect.ValueOf(r.Data).Elem()
-	updateMetadataFromValue(outputValue, "ContentLength", "file_size", res.Metadata)
-
-	lastModifiedField := outputValue.FieldByName("LastModified")
-	if lastModifiedField == (reflect.Value{}) {
-		return
-	}
-	lastModified := lastModifiedField.Elem().Interface().(time.Time)
-	res.Metadata["last_modified"] = lastModified.String()
+	//outputValue := reflect.ValueOf(r.Res).Elem()
+	//updateMetadataFromValue(outputValue.FieldByName("ContentLength"), "ContentLength", "file_size", res.Metadata)
+	//
+	//lastModifiedField := outputValue.FieldByName("LastModified")
+	//if lastModifiedField == (reflect.Value{}) {
+	//	return
+	//}
+	//lastModified := lastModifiedField.Elem().Interface().(time.Time)
+	//res.Metadata["last_modified"] = lastModified.String()
 }
 
 func handleS3PutObject(
-	r *aws.Request,
+	r *AWSCall,
 	res *protocol.Resource,
 	metadataOnly bool,
 	_ tracer.Tracer,
@@ -73,7 +115,7 @@ type s3File struct {
 }
 
 func handleS3ListObject(
-	r *aws.Request,
+	r *AWSCall,
 	res *protocol.Resource,
 	metadataOnly bool,
 	_ tracer.Tracer,
@@ -82,7 +124,7 @@ func handleS3ListObject(
 		return
 	}
 
-	outputValue := reflect.ValueOf(r.Data).Elem()
+	outputValue := reflect.ValueOf(r.Res).Elem()
 	contentsField := outputValue.FieldByName("Contents")
 	if contentsField == (reflect.Value{}) {
 		return
