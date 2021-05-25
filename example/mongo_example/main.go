2fde8b62349180a473641499274ffead5f7905e0
ronnathaniel <ronnathaniel>
2021-05-25 15:54:06 -0400

    feat(mongo): initial support, only for local InsertOne

--- example/mongo_example/main.go
+++ example/mongo_example/main.go
@@ -1,89 +1,107 @@
-//
-//package main
-//
-//import (
-//	"context"
-//	"github.com/epsagon/epsagon-go/epsagon"
-//	epsagonmongo "github.com/epsagon/epsagon-go/wrappers/mongo"
-//	"os"
-//	"time"
-//
-//	"go.mongodb.org/mongo-driver/bson"
-//	"go.mongodb.org/mongo-driver/mongo"
-//	"go.mongodb.org/mongo-driver/mongo/options"
-//	"go.mongodb.org/mongo-driver/mongo/readpref"
-//)
-//
-//type singleRes struct {
-//	Value string
-//	Key string
-//}
-//
-//func getMongo() {
-//
-//
-//	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-//	defer cancel()
-//
-//	client, err := mongo.Connect(
-//		ctx,
-//		options.Client().ApplyURI("mongodb://localhost:27017"),
-//	)
-//
-//	defer func() {
-//		if err = client.Disconnect(ctx); err != nil {
-//			panic(err)
-//		}
-//	}()
-//
-//	err = client.Ping(ctx, readpref.Primary())
-//
-//	collection := client.Database("epsagon").Collection("godev")
-//	epsagonColl := epsagonmongo.WrapMongoCollection(
-//		client.Database("epsagon"),
-//		collection,
-//		epsagon.NewTracerConfig(
-//			"mongo-dev",
-//			"38a22955-dee3-4991-8db8-afa09fc9cef6",
-//		),
-//	)
-//
-//
-//	var res singleRes
-//	err = epsagonColl.FindOne(
-//		ctx,
-//		bson.D{{"key", "value"}},
-//	).Decode(&res)
-//	if err != nil || err == mongo.ErrNoDocuments {
-//		panic(err)
-//	}
-//
-//
-//}
-//
-//
-//func main() {
-//
-//	err := os.Setenv("EPSAGON_METADATA", "FALSE")
-//	if err != nil {
-//		return
-//	}
-//	err = os.Setenv("EPSAGON_COLLECTOR_URL", "https://dev.tc.epsagon.com")
-//	if err != nil {
-//		return
-//	}
-//	err = os.Setenv("EPSAGON_DEBUG", "TRUE")
-//	if err != nil {
-//		return
-//	}
-//
-//
-//	epsagon.GoWrapper(
-//		epsagon.NewTracerConfig(
-//			"mongo-dev",
-//			"38a22955-dee3-4991-8db8-afa09fc9cef6",
-//		),
-//		getMongo,
-//	)()
-//
-//}
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/epsagon/epsagon-go/epsagon"
+	epsagonmongo "github.com/epsagon/epsagon-go/wrappers/mongo"
+	"os"
+	"time"
+
+	//"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type singleRes struct {
+	Value string
+	Key string
+}
+
+func exemplar() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(
+		ctx,
+		options.Client().ApplyURI("mongodb://localhost:27017"),
+	)
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	err = client.Ping(ctx, readpref.Primary())
+
+	db := epsagonmongo.WrapMongoDatabase(
+		client.Database("epsagon"),
+	)
+
+	coll := db.Collection("godev")
+	fmt.Println(coll)
+
+
+
+	coll.InsertOne(
+		context.Background(),
+		struct {
+			name string
+		}{ "jon" },
+	)
+
+
+	//collection := client.Database("epsagon").Collection("godev")
+	//epsagonColl := epsagonmongo.WrapMongoCollection(
+	//	client.Database("epsagon"),
+	//	collection,
+	//	//epsagon.NewTracerConfig(
+	//	//	"mongo-dev",
+	//	//	"38a22955-dee3-4991-8db8-afa09fc9cef6",
+	//	//),
+	//)
+
+
+	//var res singleRes
+	//err = epsagonColl.FindOne(
+	//	ctx,
+	//	bson.D{{"key", "value"}},
+	//).Decode(&res)
+	//if err != nil || err == mongo.ErrNoDocuments {
+	//	panic(err)
+	//}
+
+
+}
+
+
+func main() {
+
+	err := os.Setenv("EPSAGON_METADATA", "FALSE")
+	if err != nil {
+		return
+	}
+	err = os.Setenv("EPSAGON_COLLECTOR_URL", "https://dev.tc.epsagon.com")
+	if err != nil {
+		return
+	}
+	err = os.Setenv("EPSAGON_DEBUG", "TRUE")
+	if err != nil {
+		return
+	}
+
+	config := epsagon.NewTracerConfig(
+		"mongo-dev",
+		"38a22955-dee3-4991-8db8-afa09fc9cef6",
+	)
+	config.MetadataOnly = false
+	epsagon.GoWrapper(
+		config,
+		exemplar,
+	)()
+
+}
--- wrappers/mongo/mongo.go
+++ wrappers/mongo/mongo.go
@@ -7,6 +7,9 @@ import (
 	"github.com/epsagon/epsagon-go/epsagon"
 	"github.com/epsagon/epsagon-go/protocol"
 	"github.com/epsagon/epsagon-go/tracer"
+	//"time"
+
+	//"github.com/epsagon/epsagon-go/tracer"
 	"github.com/google/uuid"
 
 	//"github.com/golang/protobuf/ptypes/any"
@@ -14,17 +17,25 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type MongoCollectionWrapper struct {
-	Database *mongo.Database
-	Collection *mongo.Collection
-	Config *epsagon.Config
-}
+
+//type MongoCall struct {
+//	opName string
+//	startTime *time.Time
+//	database *MongoDatabaseWrapper
+//	collection *MongoCollectionWrapper
+//
+//}
 
 type MongoDatabaseWrapper struct {
 	Database *mongo.Database
 	Config *epsagon.Config
 }
 
+type MongoCollectionWrapper struct {
+	Database *mongo.Database
+	Collection *mongo.Collection
+	Config *epsagon.Config
+}
 func WrapMongoCollection(database *mongo.Database, collection *mongo.Collection, config *epsagon.Config) *MongoCollectionWrapper {
 	return &MongoCollectionWrapper{
 		Database: database,
@@ -33,10 +44,10 @@ func WrapMongoCollection(database *mongo.Database, collection *mongo.Collection,
 	}
 }
 
-func wrapMongoDatabase(database *mongo.Database, config *epsagon.Config) *MongoDatabaseWrapper {
+func WrapMongoDatabase(database *mongo.Database) *MongoDatabaseWrapper {
 	return &MongoDatabaseWrapper{
 		Database: database,
-		Config: config,
+		//Config: config,
 	}
 }
 
@@ -50,80 +61,145 @@ func (db *MongoDatabaseWrapper) Name() string {
 
 func (db *MongoDatabaseWrapper) Collection(name string, opts ...*options.CollectionOptions) *MongoCollectionWrapper {
 	return &MongoCollectionWrapper{
+		Database: db.Database,
 		Collection: db.Database.Collection(name, opts...),
 		Config: db.Config,
 	}
 }
 
+func completeMongoEvent(res *protocol.Resource, startTime float64, args ...context.Context) {
+	//traceArgs := make([]context.Context, 1)
+	currentTracer := epsagon.ExtractTracer(args)
+	//currentTracer.
 
-func (coll *MongoCollectionWrapper) FindOne(
-		ctx context.Context,
-		filter interface{},
-		opts ...*options.FindOneOptions) *mongo.SingleResult {
-
-	findOne := func() *mongo.SingleResult{
-		res := coll.Collection.FindOne(
-			ctx,
-			filter,
-			opts...,
-		)
-
-		fmt.Println(res)
-		return res
-	}
-
-
-	return wrapCall(findOne, coll.Config)
-}
-
-func wrapCall(
-	handler func() *mongo.SingleResult,
-	config *epsagon.Config,
-	//opts ...any.Any,
-) *mongo.SingleResult{
-	//eventTracer := tracer.CreateTracer(&config.Config)
-	var arg context.Context
-	currentTracer := epsagon.ExtractTracer(arg)
-
-	eventTracer.Start()
-	fmt.Println("Started Tracer")
-
-	defer func() {
-		eventTracer.SendStopSignal()
-		fmt.Println("stopped")
-	}()
-
-	fmt.Println("adding event")
-	eventTracer.AddEvent(
-		createMongoEvent(
-			"MyColl",
-			"FindOne",
-
-		),
-	)
-	fmt.Println("added event")
-
-
-	return handler()
-
-}
-
-func createMongoEvent(
-	name, method string,
-	//err error
-	) *protocol.Event {
-
-	return &protocol.Event{
-		Id: "mongo.driver-" + uuid.New().String(),
-		Origin: "mongo.driver",
+	endTime := tracer.GetTimestamp()
+	event := &protocol.Event{
+		Id: "mongodb-" + uuid.New().String(),
+		Origin: "mongodb",
 		ErrorCode: protocol.ErrorCode_OK,
-		Resource: &protocol.Resource{
-			Name:	name,
-			Type:	"mongo",
-			Operation: method,
-			Metadata: map[string]string{},
-		},
+		Resource: res,
+		StartTime: startTime,
 	}
+	event.Duration = endTime - event.StartTime
+	currentTracer.AddEvent(event)
+
 }
+
+func createMongoResource(opName string, coll *MongoCollectionWrapper) *protocol.Resource {
+	res := &protocol.Resource{
+		Name: coll.Database.Name() + "." + coll.Collection.Name(),
+		Type: "mongodb",
+		Operation: opName,
+		Metadata: make(map[string]string),
+
+	}
+
+	return res
+}
+
+func (coll *MongoCollectionWrapper) InsertOne(args ...interface{}) (interface{}, error) {
+
+
+	if len(args) < 2 {
+		return nil, fmt.Errorf("Not enough Args")
+	}
+
+	startTime := tracer.GetTimestamp()
+	//insert := func() ([]interface{}, error)
+	res := createMongoResource("InsertOne", coll)
+	//return coll.Collection.InsertOne(args...)
+
+	completeMongoEvent(res, startTime)
+
+	fmt.Println(args[0])
+	return nil, nil
+}
+
+
+//func (coll *MongoCollectionWrapper) FindOne(
+//		ctx context.Context,
+//		filter interface{},
+//		opts ...*options.FindOneOptions) *mongo.SingleResult {
+//
+//	findOne := func() *mongo.SingleResult{
+//		res := coll.Collection.FindOne(
+//			ctx,
+//			filter,
+//			opts...,
+//		)
+//
+//		fmt.Println(res)
+//		return res
+//	}
+//
+//
+//	return wrapCall(findOne, coll.Config)
+//}
+
+//func wrapCall(
+//	handler func() *mongo.SingleResult,
+//	config *epsagon.Config,
+//	//opts ...any.Any,
+//	args ...context.Context,
+//) *mongo.SingleResult {
+//	//eventTracer := tracer.CreateTracer(&config.Config)
+//	//var arg context.Context
+//	currentTracer := epsagon.ExtractTracer(args)
+//
+//	startTime := tracer.GetTimestamp()
+//
+//
+//	event := protocol.Event{
+//		Id:                   uuid.NewString(),
+//		StartTime:            startTime,
+//		Resource:             nil,
+//		Origin:               "mongo",
+//		Duration:             0,
+//		ErrorCode:            0,
+//		Exception:            nil,
+//		XXX_NoUnkeyedLiteral: struct{}{},
+//		XXX_unrecognized:     nil,
+//		XXX_sizecache:        0,
+//	}
+//
+//	fmt.Println("Started Tracer")
+//
+//	//defer func() {
+//	//	currentTracer.SendStopSignal()
+//	//	fmt.Println("stopped")
+//	//}()
+//
+//	fmt.Println("adding event")
+//	currentTracer.AddEvent(
+//		createMongoEvent(
+//			"MyColl",
+//			"FindOne",
+//
+//		),
+//	)
+//	fmt.Println("added event")
+//
+//
+//	return handler()
+//
+//}
+
+//func createMongoEvent(
+//	name, method string,
+//	//err error
+//	) *protocol.Event {
+//
+//	return &protocol.Event{
+//		Id: "mongo.driver-" + uuid.New().String(),
+//		Origin: "mongo.driver",
+//		ErrorCode: protocol.ErrorCode_OK,
+//		Resource: &protocol.Resource{
+//			Name:	name,
+//			Type:	"mongo",
+//			Operation: method,
+//			Metadata: map[string]string{},
+//		},
+//	}
+//}
 
 
