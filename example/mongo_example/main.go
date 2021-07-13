866bda8ee8bb386b6b62a6a7ca3a49ce9891ebbf
Ron Nathaniel <45116635+ronnathaniel@users.noreply.github.com>
2021-07-13 12:45:17 +0300

    Feat(mongo): Add support (#94)
    Added Mongo Collection Wrapper to Trace through all Mongo operations.
    
    Co-authored-by: ronnathaniel <ronnathaniel>

---
+++ example/mongo_example/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/epsagon/epsagon-go/epsagon"
+	epsagonmongo "github.com/epsagon/epsagon-go/wrappers/mongo"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func dbAPI() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(
+		ctx,
+		options.Client().ApplyURI("mongodb://localhost:27017"),
+	)
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	err = client.Ping(ctx, readpref.Primary())
+
+	db := client.Database("DB")
+
+	// WRAP THE MONGO COLLECTION with WrapMongoCollection()
+	coll := epsagonmongo.WrapMongoCollection(
+		db.Collection("COLL"),
+	)
+
+	type doc struct {
+		Name string
+	}
+	var res interface{}
+
+	fmt.Println("##InsertOne")
+	_, err = coll.InsertOne(
+		context.Background(),
+		doc{Name: "bon"},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("##InsertMany")
+	_, err = coll.InsertMany(
+		context.Background(),
+		[]interface{}{
+			bson.D{
+				{Key: "name", Value: "hello"},
+				{Key: "age", Value: "33"},
+			},
+			bson.D{
+				{Key: "name", Value: "world"},
+				{Key: "age", Value: "44"},
+			},
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("##FindOne")
+	coll.FindOne(
+		context.Background(),
+		bson.D{{Key: "name", Value: "bon"}},
+	)
+
+	fmt.Println("##Find")
+	coll.Find(context.Background(), bson.M{})
+
+	fmt.Println("##Aggregate")
+	res, err = coll.Aggregate(
+		context.Background(),
+		mongo.Pipeline{
+			bson.D{{Key: "$match", Value: bson.D{{Key: "name", Value: "bon"}}}},
+		},
+	)
+	if err != nil || err == mongo.ErrNoDocuments {
+		panic(err)
+	}
+
+	fmt.Println("##CountDocuments")
+	res, err = coll.CountDocuments(
+		context.Background(),
+		bson.D{{Key: "name", Value: "bon"}},
+	)
+	fmt.Println(res)
+	if err != nil || err == mongo.ErrNoDocuments {
+		panic(err)
+	}
+
+	fmt.Println("##DeleteOne")
+	res, err = coll.DeleteOne(
+		context.Background(),
+		bson.D{{Key: "name", Value: "bon"}},
+	)
+	fmt.Println(res)
+	if err != nil || err == mongo.ErrNoDocuments {
+		panic(err)
+	}
+
+	fmt.Println("##UpdateOne")
+	res, err = coll.UpdateOne(
+		context.Background(),
+		bson.D{{Key: "name", Value: "bon"}},
+		bson.D{{Key: "$set", Value: bson.D{{Key: "name", Value: "son"}}}},
+	)
+	fmt.Println(res)
+	if err != nil || err == mongo.ErrNoDocuments {
+		panic(err)
+	}
+}
+
+func main() {
+
+	config := epsagon.NewTracerConfig(
+		"skate-shop",
+		"token",
+	)
+	config.MetadataOnly = false
+	config.Debug = true
+
+	epsagon.GoWrapper(
+		config,
+		dbAPI,
+	)()
+
+}
---
+++ wrappers/mongo/mongo_test.go
@@ -0,0 +1,174 @@
+package epsagonmongo
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/benweissmann/memongo"
+	"github.com/epsagon/epsagon-go/epsagon"
+	"github.com/epsagon/epsagon-go/protocol"
+	"github.com/epsagon/epsagon-go/tracer"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func TestMongoWrapper(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Mongo Driver Test Suite")
+}
+
+var _ = Describe("mongo_wrapper", func() {
+	Describe("CollectionWrapper", func() {
+		var (
+			mongoServer        *memongo.Server
+			mongoOptions       *memongo.Options
+			started            chan bool
+			testConf           *epsagon.Config
+			events             []*protocol.Event
+			exceptions         []*protocol.Exception
+			wrapper            *MongoCollectionWrapper
+			testContext        context.Context
+			testDatabaseName   string
+			testCollectionName string
+			cancel             func()
+		)
+		BeforeEach(func() {
+			started = make(chan bool)
+			// start server goroutine, runs in background until block
+			go func() {
+				mongoOptions = &memongo.Options{
+					MongoVersion:   "4.2.0",
+					StartupTimeout: 5 * time.Second,
+				}
+				mongoServer, _ = memongo.StartWithOptions(mongoOptions)
+				started <- true
+			}()
+
+			testConf = &epsagon.Config{Config: tracer.Config{
+				Disable:  true,
+				TestMode: true,
+			}}
+			events = make([]*protocol.Event, 0)
+			exceptions = make([]*protocol.Exception, 0)
+			tracer.GlobalTracer = &tracer.MockedEpsagonTracer{
+				Events:     &events,
+				Exceptions: &exceptions,
+				Labels:     make(map[string]interface{}),
+				Config:     &testConf.Config,
+			}
+
+			testCollectionName = "collectionName"
+			testDatabaseName = "databaseName"
+			testContext, cancel = context.WithTimeout(context.Background(), 2*time.Second)
+
+			// blocking await until server is started
+			select {
+			case <-started:
+				break
+			}
+			client, _ := mongo.Connect(testContext, options.Client().ApplyURI(mongoServer.URI()))
+			wrapper = &MongoCollectionWrapper{
+				collection: client.Database(testDatabaseName).Collection(testCollectionName),
+				tracer:     tracer.GlobalTracer,
+			}
+		})
+		AfterEach(func() {
+			mongoServer.Stop()
+			cancel()
+		})
+		Context("Writing DB", func() {
+			It("calls InsertOne", func() {
+				_, err := wrapper.InsertOne(context.Background(), struct {
+					Name string
+				}{"TestName"})
+				Expect(err).To(BeNil())
+			})
+			It("calls InsertMany", func() {
+				_, err := wrapper.InsertMany(context.Background(), []interface{}{
+					bson.D{
+						{Key: "name", Value: "hello"},
+						{Key: "age", Value: "33"},
+					},
+					bson.D{
+						{Key: "name", Value: "world"},
+						{Key: "age", Value: "44"},
+					},
+				})
+				Expect(err).To(BeNil())
+			})
+		})
+		Context("Reading DB", func() {
+			It("calls InsertOne and FindOne", func() {
+				type doc struct {
+					Name string
+				}
+				reqDoc := doc{Name: "TestName"}
+				resDoc := doc{}
+
+				_, err := wrapper.InsertOne(context.Background(), reqDoc)
+				Expect(err).To(BeNil())
+
+				response := wrapper.FindOne(
+					context.Background(),
+					bson.D{{Key: "name", Value: "TestName"}},
+				)
+
+				response.Decode(&resDoc)
+				Expect(reqDoc).To(Equal(resDoc))
+				Expect(response.Err()).To(BeNil())
+
+			})
+			It("calls InsertMany and Find", func() {
+				type doc struct {
+					Name string
+				}
+				docs := []interface{}{
+					bson.D{
+						{Key: "name", Value: "hello"},
+						{Key: "age", Value: "33"},
+					},
+					bson.D{
+						{Key: "name", Value: "world"},
+						{Key: "age", Value: "44"},
+					},
+				}
+
+				_, err := wrapper.InsertMany(context.Background(), docs)
+				Expect(err).To(BeNil())
+
+				cur, err := wrapper.Find(
+					context.Background(),
+					bson.M{},
+				)
+				Expect(err).To(BeNil())
+
+
+				readCursor(cur)
+			})
+			It("calls CountDocuments", func() {
+				docs := []interface{}{
+					bson.D{
+						{Key: "name", Value: "hello"},
+						{Key: "age", Value: "33"},
+					},
+					bson.D{
+						{Key: "name", Value: "world"},
+						{Key: "age", Value: "44"},
+					},
+				}
+
+				wrapper.InsertMany(context.Background(), docs)
+				res, err := wrapper.CountDocuments(
+					context.Background(),
+					bson.D{{}},
+				)
+				Expect(err).To(BeNil())
+				Expect(res).To(Equal(int64(2)))
+			})
+		})
+	})
+})
